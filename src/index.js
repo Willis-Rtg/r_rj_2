7346a99a96384b6c6dbb9cee221ad892de494823
= <kshwan1023@gmail.com>
2021-03-22 14:43:37 +0900

    components => Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import App from "./components/App";
+import App from "./Components/App";
 import Client from "./Apollo/Client";
 
 ReactDOM.render(
