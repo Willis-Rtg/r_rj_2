68561a0991b7178497c81835578127aa5c43751e
Willis Kim <kshwan.1023@gmail.com>
2021-03-22 16:15:16 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import App from "./Components/App";
+import App from "./components/App";
 import Client from "./Apollo/Client";
 
 ReactDOM.render(
